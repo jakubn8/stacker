f668e8a8e251c7fa5963884165d23d28937847df
Jakub Nowak <jakub8.nowak8@gmail.com>
2025-12-10 18:45:45 +0000

    Temp: Add _TESTING suffix to bypass billing check for testing

--- lib/whop-sdk.ts
+++ lib/whop-sdk.ts
@@ -65,7 +65,8 @@ export const whopsdk = {
 
 // Stacker's billing company ID (where Stacker receives 5% fee payments from sellers)
 // This must be DIFFERENT from any seller's company ID
-export const STACKER_COMPANY_ID = process.env.WHOP_COMPANY_ID || "";
+// TODO: Remove "_TESTING" suffix when app goes live
+export const STACKER_COMPANY_ID = process.env.WHOP_COMPANY_ID ? `${process.env.WHOP_COMPANY_ID}_TESTING` : "";
 
 // Stacker's App ID
 export const STACKER_APP_ID = process.env.WHOP_APP_ID || "";
